a0c16d41f56749fb952376d40e2e5be7e49c2e8f
mikakane <mikakane2@gmail.com>
2016-05-28 15:48:36 +0900

    例外処理を追加

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -45,6 +46,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        $response = new JsonResponse([
+            "message" => $e->getMessage()
+        ],500,[]);
+
+        return $response;
     }
 }
